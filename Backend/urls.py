0d6665e5edfa45dee8a0e2aa29502c3b27d91df7
KamilB02 <kamilekbordowy@o2.pl>
2024-05-15 14:59:06 +0200

    frameworkupdate

--- Backend/urls.py
+++ Backend/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from api.views import CreateUserView
-from rest_framework_simple
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
