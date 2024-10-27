9dc21bad18561aa8af9094c6829c47012ab5f226
kamil <kamilekbordowy02@gmail.com>
2024-10-27 20:32:01 +0100

    user req calculator

--- api/services/genetic_algorithm.py
+++ api/services/genetic_algorithm.py
@@ -1,4 +1,83 @@
+import random
+
+# Przykładowe przepisy
+available_recipes = [
+    {'name': 'Salatka z kurczakiem', 'calories': 400, 'macros': {'protein': 30, 'carbs': 20, 'fats': 15}},
+    {'name': 'Zupa jarzynowa', 'calories': 150, 'macros': {'protein': 5, 'carbs': 25, 'fats': 5}},
+    # Dodaj więcej przepisów
+]
 
 
+# Klasa reprezentująca plan dietetyczny
+class DietPlan:
+    def __init__(self, recipes):
+        self.recipes = recipes
+        self.calories = sum(recipe['calories'] for recipe in recipes)
+        self.macros = self.calculate_macros()
 
-def genetic_algorithm(user_requirements, generations=100):
+    def calculate_macros(self):
+        total_macros = {'protein': 0, 'carbs': 0, 'fats': 0}
+        for recipe in self.recipes:
+            for macro, value in recipe['macros'].items():
+                total_macros[macro] += value
+        return total_macros
+
+
+# Generowanie początkowej populacji
+def generate_initial_population(user_requirements, size=20):
+    population = []
+    for _ in range(size):
+        selected_recipes = random.sample(available_recipes, k=7)  # Wybierz 7 losowych przepisów
+        population.append(DietPlan(selected_recipes))
+    return population
+
+
+# Funkcja dopasowania
+def fitness(plan, user_requirements):
+    score = 0
+    # Przykład: dodaj punkty za kalorie i makroskładniki
+    if plan.calories <= user_requirements['calories']:
+        score += 1
+    # Dodaj inne kryteria oceny
+    return score
+
+
+# Algorytm genetyczny
+def genetic_algorithm(user_requirements, generations=10):
+    population = generate_initial_population(user_requirements)
+
+    for generation in range(generations):
+        population.sort(key=lambda x: fitness(x, user_requirements), reverse=True)
+        new_population = population[:10]  # Najlepsze 10 planów
+
+        # Krzyżowanie
+        while len(new_population) < 20:
+            parent1, parent2 = random.sample(new_population[:10], 2)
+            crossover_recipes = random.sample(parent1.recipes + parent2.recipes, k=7)
+            new_population.append(DietPlan(crossover_recipes))
+
+        # Mutacja
+        for plan in new_population:
+            if random.random() < 0.1:  # 10% szans na mutację
+                new_recipe = random.choice(available_recipes)
+                plan.recipes[random.randint(0, 6)] = new_recipe  # Zmień losowy przepis
+                plan.calories = sum(recipe['calories'] for recipe in plan.recipes)  # Zaktualizuj kalorie
+                plan.macros = plan.calculate_macros()  # Zaktualizuj makroskładniki
+
+        population = new_population
+
+    return population[0]  # Zwróć najlepszy plan
+
+
+# Przykładowe wymagania użytkownika
+user_requirements = {
+    'calories': 2000,
+    # Dodaj inne wymagania
+}
+
+best_plan = genetic_algorithm(user_requirements)
+print("Najlepszy plan dietetyczny:")
+for recipe in best_plan.recipes:
+    print(recipe['name'])
+
+
---
+++ api/services/user_required.py
@@ -0,0 +1,77 @@
+user_info = {
+    'weight': 87,
+    'height': 179,
+    'age': 22,
+    'gender': 'man',
+    'physical_activity_at_work': 0,
+    'physical_activity_in_free_time': 3,
+    'objective': 'less',
+}
+
+
+def calculate_calories(user_info):
+    weight = user_info['weight']
+    height = user_info['height']
+    age = user_info['age']
+    gender = user_info['gender']
+    activity_work = user_info['physical_activity_at_work']
+    activity_free_time = user_info['physical_activity_in_free_time']
+    objective = user_info['objective']
+
+    if gender == 'man':
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5  # Mifflin-St Jeor Equation for man
+    elif gender == 'woman':
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161 # Mifflin-St Jeor Equation for woman
+    else:
+        raise ValueError("Niepoprawna wartość płci, wybierz 'man' lub 'woman'.")
+
+    activity_factors = [
+        [1.30, 1.35, 1.40, 1.50],
+        [1.40, 1.45, 1.50, 1.60],
+        [1.50, 1.55, 1.60, 1.70],
+        [1.60, 1.65, 1.70, 1.80],
+        [1.70, 1.75, 1.80, 1.90],
+    ]
+
+    total_calories = round(bmr * activity_factors[activity_free_time][activity_work])
+
+    if objective == 'less':
+        total_calories = total_calories - 300
+    elif objective == 'same':
+        total_calories = total_calories
+    elif objective == 'more':
+        total_calories = total_calories + 300
+
+    return total_calories
+
+
+def calculate_macro(user_info):
+    protein = 0
+    carbs = 0
+    fats = 0
+    objective = user_info['objective']
+
+    calories = calculate_calories(user_info)
+
+    if objective == 'less':
+        protein = round((calories * 0.25)/4)
+        carbs = round((calories * 0.50)/4)
+        fats = round((calories * 0.25)/9)
+    elif objective == 'same':
+        protein = round((calories * 0.15)/4)
+        carbs = round((calories * 0.55)/4)
+        fats = round((calories * 0.30)/9)
+    elif objective == 'more':
+        protein = round((calories * 0.20)/4)
+        carbs = round((calories * 0.50)/4)
+        fats = round((calories * 0.30)/9)
+
+    return protein, carbs, fats
+
+
+daily_calories = calculate_calories(user_info)
+daily_macro = calculate_macro(user_info)
+print("Dzienne zapotrzebowanie kaloryczne wynosi:", daily_calories, "kcal")
+print("Dzienne zapotrzebowanie protein:", daily_macro[0], "g")
+print("Dzienne zapotrzebowanie carbs:", daily_macro[1], "g")
+print("Dzienne zapotrzebowanie fats:", daily_macro[2], "g")
